0e1c784d35ff3149660dcfd6fd72d33f4c55a39d
Michael Benzinger <michaelbenzinger3@gmail.com>
2021-11-24 13:43:06 -0600

    Display products on All Products page

--- src/components/All.js
+++ src/components/All.js
@@ -1,8 +1,87 @@
+import { UNSPLASH_ACCESS_KEY } from '../config';
+import { useState, useEffect } from 'react';
+
+import '../styles/All.css';
+import { myProducts } from '../products';
+
 function All() {
+  const [products, setProducts] = useState(myProducts);
+
+  useEffect(() => {
+    if (products.find(product => !product.src)) {
+      getPhotos();
+    }
+  });
+
+  function getPhotos() {
+    console.log('Querying API');
+
+    products.forEach(product => {
+      fetch(
+        `https://api.unsplash.com/photos/${product.id}/?client_id=${UNSPLASH_ACCESS_KEY}`
+      )
+        .then(res => res.json())
+        .then(json => {
+          setProducts(
+            products.map(product => {
+              if (product.id === json.id) {
+                product.src = json.urls.regular;
+              }
+              return product;
+            })
+          );
+        });
+    });
+
+    // Promise.all(
+    //   myProducts.map(product => {
+    //     return new Promise((resolve, reject) => {
+    //       fetch(
+    //         `https://api.unsplash.com/photos/${product.id}/?client_id=${UNSPLASH_ACCESS_KEY}`
+    //       )
+    //         .then(res => res.json())
+    //         .then(json =>
+    //           resolve({ productInfo: product.productInfo, json: json })
+    //         );
+    //     });
+    //   })
+    // ).then(values => {
+    //   setProducts(
+    //     values.map(product => {
+    //       return {
+    //         productInfo: product.productInfo,
+    //         id: product.json.id,
+    //         src: product.json.urls.regular,
+    //       };
+    //     })
+    //   );
+    // });
+  }
+
   return (
     <div className="all-component">
       <div className="contained">
         <h1>All Products</h1>
+        <div className="products-grid">
+          {products.map(product => (
+            <div className="product-card" key={product.id}>
+              <div className="img-container">
+                {product.src ? (
+                  <img alt={product.productInfo.name} src={product.src} />
+                ) : (
+                  <div className="product-card__placeholder" />
+                )}
+              </div>
+              <h3 className="product-card__name">{product.productInfo.name}</h3>
+              <p className="product-card__category">
+                {product.productInfo.brand} – {product.productInfo.category}
+              </p>
+              <p className="product-card__price">
+                ${product.productInfo.price}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,38 @@
+export const myProducts = [
+  {
+    id: 'LxVxPA1LOVM',
+    productInfo: {
+      name: 'Electric Air – Gray',
+      brand: 'Nike',
+      category: 'Running',
+      price: 90,
+    },
+  },
+  {
+    id: 'LG88A2XgIXY',
+    productInfo: {
+      name: 'Lanemaster – Orange',
+      brand: 'Nike',
+      category: 'Running',
+      price: 75,
+    },
+  },
+  {
+    id: 'vraH9m7hfHU',
+    productInfo: {
+      name: 'Air Quotes',
+      brand: 'Nike',
+      category: 'Skate',
+      price: 110,
+    },
+  },
+  {
+    id: 'HcqA34-uWo4',
+    productInfo: {
+      name: 'Straight Shot Low',
+      brand: 'Puma',
+      category: 'Skate',
+      price: 70,
+    },
+  },
+];
